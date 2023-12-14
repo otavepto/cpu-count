884e981b66c9da4f12c82c0968d221cb78d0c576
a <e>
2023-12-14 19:43:29 +0200

    add all files

---
+++ cpu_count/cpu_count.cpp
@@ -0,0 +1,13 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+int main()
+{
+    SYSTEM_INFO sys_info;
+    GetNativeSystemInfo(&sys_info);
+
+    return sys_info.dwNumberOfProcessors;
+}
+
+
